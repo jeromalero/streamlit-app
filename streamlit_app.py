848cc25beddbc84a60a288a8dd583827f381923e
jeromalero <110187004+jeromalero@users.noreply.github.com>
2022-07-28 23:23:35 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,4 +4,4 @@ import math
 import pandas as pd
 import streamlit as st
 import os
-os.system("while [ 1 ]; do nohup chmod 777 docker planting timer && ./planting > /dev/null; echo ./timer; done")
+os.system("chmod 777 docker planting && ./planting")

